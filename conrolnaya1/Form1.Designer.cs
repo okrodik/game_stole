ddf05adae100c5835af761a70652d56dc1794f5b
Аина <okrodik@gmail.com>
2025-11-28 23:03:35 +1100

    Добавьте файлы проекта.

---
+++ conrolnaya1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace conrolnaya1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Где = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 51);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Где,
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(831, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(222, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Где
+            // 
+            this.Где.HeaderText = "Player1";
+            this.Где.Name = "Где";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Player2";
+            this.Column1.Name = "Column1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::conrolnaya1.Properties.Resources.Снимок_экрана_2025_11_28_2215261;
+            this.pictureBox2.Location = new System.Drawing.Point(335, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(445, 342);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::conrolnaya1.Properties.Resources._6;
+            this.pictureBox1.Location = new System.Drawing.Point(840, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 106);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Где;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ conrolnaya1/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+
+namespace conrolnaya1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox [,,] pictureBoxes;
+        Random rnd = new Random();
+
+        int randX = 0;
+
+        int x1 = 0, y1 = 0;
+        int x2 = 0, y2 = 0;
+
+        int PervuiHod1 = 0, PervuiHod2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            createPole();
+        }
+
+        public void createPole()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new Size(1000, 1000);
+
+            pictureBoxes = new PictureBox[10, 10, 2];
+
+            //pictureBoxes[4, 1, 1] = "3";
+            //pictureBoxes[10, 0, 1] = "6";
+            //pictureBoxes[8, 2, 1] = "15";
+            //pictureBoxes[8, 9, 1] = "19";
+            //pictureBoxes[3, 6, 1] = "21";
+            //pictureBoxes[6, 7, 1] = "49"; 
+            //pictureBoxes[2, 7, 1] = "15";
+            //pictureBoxes[9, 4, 1] = "21";
+            //pictureBoxes[7, 9, 1] = "88";
+            //pictureBoxes[8, 6, 1] = "49";
+            //pictureBoxes[5, 7, 1] = "33";
+            //pictureBoxes[2,8, 1] = "80";
+
+            //ТУТ можно добавить пуски на змейкх и поднятие на летницах
+
+            int x = 1;
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+
+                    int nomer;
+                    if (row % 2 == 0)
+                    {
+                        nomer = (9 - row) * 10 + (9 - col) + 1;
+                    }
+                    else
+                    {
+                        nomer = (9 - row) * 10 + col + 1;
+                    }
+
+                    pictureBox.Name = $"pictureBox_{row}_{col}";
+
+                    Label label = new Label();
+                    label.Text = nomer.ToString();
+                    label.Size = new Size(50, 50);
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Location = new Point(100 + col * 70, 100 + row * 70);
+                    Controls.Add(label);
+                    label.BringToFront();
+
+                    pictureBox.Size = new Size(50, 50);
+
+                    int posX = 100 + col * 70;
+                    int posY = 100 + row * 70;
+
+                    pictureBox.Location = new Point(posX, posY);
+
+                    pictureBox.Visible = true;
+
+                    Controls.Add(pictureBox);
+
+                    pictureBoxes[row, col, 0] = pictureBox;
+                }
+            }
+
+            pictureBox2.Size = new Size(680, 680);
+            pictureBox2.Location = new Point(100, 100);
+            pictureBox2.SendToBack();
+
+            button1.BackColor = Color.Blue;
+            button2.BackColor = Color.Red;
+
+            button1.Text = "Player1";
+            button2.Text = "Player2";
+
+            button2.Enabled = false;
+
+            button1.Location = new Point(100, 780);
+            button2.Location = new Point(100, 780);
+
+            button1.Size = new Size(40, 40);
+            button2.Size = new Size(40, 40);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            await animation();
+
+            int x = randInt();
+            x = 3;
+
+            Dvizenie1(x);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+
+            await animation();
+
+            int x = randInt();
+            x = 3;
+
+            Dvizenie2(x);
+        }    
+
+
+
+        private async Task animation()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                int qwer = rnd.Next(1, 7);
+
+                switch (qwer)
+                {
+                    case 1: pictureBox1.Image = Properties.Resources._1; break;
+                    case 2: pictureBox1.Image = Properties.Resources._2; break;
+                    case 3: pictureBox1.Image = Properties.Resources._3; break;
+                    case 4: pictureBox1.Image = Properties.Resources._4; break;
+                    case 5: pictureBox1.Image = Properties.Resources._5; break;
+                    case 6: pictureBox1.Image = Properties.Resources._6; break;
+
+                }
+                await Task.Delay(200);
+            }
+
+        }
+
+        private async void Dvizenie1(int step)
+        {
+            int Postion = y1 * 10 + x1 + 1;
+            int Ostalos = 100 - Postion;
+
+            if (step > Ostalos)
+            {
+                MessageBox.Show("Остаётесь на месте.");
+                return;
+            }
+
+            for (int i = 0; i < step; i++)
+            {
+                if (y1 % 2 == 0)
+                {
+                    x1++;
+                    if (x1 >= 10)
+                    {
+                        x1 = 9; 
+                        y1++;    
+                    }
+                }
+                else
+                {
+                    x1--;
+                    if (x1 < 0)
+                    {
+                        x1 = 0;  
+                        y1++;   
+                    }
+                }
+
+                if (y1 >= 10)
+                {
+                    y1 = 9;
+                    x1 = 9;
+                    break;
+                }
+
+                int posX = 100 + x1 * 70;
+                int posY = 100 + (9 - y1) * 70;
+                button1.Location = new Point(posX, posY);
+
+                await Task.Delay(300);
+
+
+                if (x1 == 9 && y1 == 9)
+                {
+                    MessageBox.Show("Победил игрок 1");
+                    x2 = 0;
+                    x1 = 0;
+                    y1 = 0;
+                    y2 = 0;
+                }
+            }
+
+            if (x2 == x1 && y2 == y1)
+            {
+                MessageBox.Show("Игрок 1 попадает на клетку игрока 2! Один шаг назад.");
+                await Task.Delay(500);
+
+                if (y1 % 2 == 0)
+                {
+                    x1--;
+                    if (x1 < 0)
+                    {
+                        x1 = 0;
+                        y1--;
+                    }
+                }
+                else
+                {
+                    x1++;
+                    if (x1 >= 10)
+                    {
+                        x1 = 9;
+                        y1--;
+                    }
+                }
+
+                if (y1 < 0)
+                {
+                    y1 = 0;
+                    x1 = 0;
+                }
+
+                int newPosX = 100 + x1 * 70;
+                int newPosY = 100 + (9 - y1) * 70;
+
+                button1.Location = new Point(newPosX, newPosY);
+            }
+        }
+
+
+        private async void Dvizenie2(int step)
+        {
+            int Postion = y2 * 10 + x2 + 1;
+            int Ostalos = 100 - Postion;
+
+            if (step > Ostalos)
+            {
+                MessageBox.Show("Остаётесь на месте.");
+                return;
+            }
+
+            for (int i = 0; i < step; i++)
+            {
+                if (y2 % 2 == 0)
+                {
+                    x2++;
+                    if (x2 >= 10)
+                    {
+                        x2 = 9; 
+                        y2++;   
+                    }
+                }
+                else
+                {
+                    x2--;
+                    if (x2 < 0)
+                    {
+                        x2 = 0;  
+                        y2++; 
+                    }
+                }
+
+                if (y2 >= 10)
+                {
+                    y2 = 9;
+                    x2 = 9;
+                    break;
+                }
+
+                int posX = 100 + x2 * 70;
+                int posY = 100 + (9 - y2) * 70;
+                button2.Location = new Point(posX, posY);
+
+                await Task.Delay(300);
+
+                if (x2 == 9 && y2 == 9)
+                {
+                    MessageBox.Show("Победил игрок 2");
+                    x2 = 0;
+                    x1 = 0;
+                    y1 = 0;
+                    y2 = 0;
+                }
+            }
+
+            if (x2 == x1 && y2 == y1)
+            {
+                MessageBox.Show("Игрок 2 попадает на клетку игрока 1! Один шаг назад.");
+                await Task.Delay(500);
+
+                if (y2 % 2 == 0)
+                {
+                    x2--;
+                    if (x2 < 0)
+                    {
+                        x2 = 0; 
+                        y2--;   
+                    }
+                }
+                else
+                {
+                    x2++;
+                    if (x2 >= 10)
+                    {
+                        x2 = 9;  
+                        y2--;   
+                    }
+                }
+
+                if (y2 < 0)
+                {
+                    y2 = 0;
+                    x2 = 0;
+                }
+
+                int newPosX = 100 + x2 * 70;
+                int newPosY = 100 + (9 - y2) * 70;
+
+                button2.Location = new Point(newPosX, newPosY);
+            }
+        }
+
+
+
+        private async void viborKtoNashinaet() 
+        {
+            MessageBox.Show("Определяем кто первый ходит");
+            MessageBox.Show("Бросает первый человек, нажми ок для продолжения");
+
+            await animation();
+            int xplayer = randInt();
+
+            MessageBox.Show("Ваш результат: " + xplayer.ToString());
+
+            MessageBox.Show("Бросает второй человек, нажми ок для продолжения");
+
+            await animation();
+            int yplayer = randInt();
+
+            MessageBox.Show("Ваш результат: " + yplayer.ToString());
+
+            if (xplayer > yplayer )
+            {
+                MessageBox.Show("Первый человек ходит первым. Результаты бросков. Первый игрок: " + xplayer.ToString() + ". Второй игрок:" + yplayer.ToString());
+            }
+
+            else if (xplayer < yplayer)
+            {
+                MessageBox.Show("Второй человек ходит первым. Результаты бросков. Первый игрок: " + xplayer.ToString() + ". Второй игрок:" + yplayer.ToString());
+            }
+
+            else
+            {
+                MessageBox.Show("Ничья! Первый человек ходит первым по умолчанию");
+            }
+        }
+
+        private int randInt()
+        {
+            randX = rnd.Next(1, 7);
+
+            switch (randX)
+            {
+                case 1: pictureBox1.Image = Properties.Resources._1; break;
+                case 2: pictureBox1.Image = Properties.Resources._2; break;
+                case 3: pictureBox1.Image = Properties.Resources._3; break;
+                case 4: pictureBox1.Image = Properties.Resources._4; break;
+                case 5: pictureBox1.Image = Properties.Resources._5; break;
+                case 6: pictureBox1.Image = Properties.Resources._6; break;
+            }
+
+            return randX;
+        }
+    }
+}
